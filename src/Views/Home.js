ca05e69db7dfde98f56d2cd0708fb08511570b88
M.Tayyab Qamar <tayyab089@gmail.com>
2022-02-09 11:12:39 +0500

    Image Piker Debugging

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,10 +1,25 @@
-import React from "react";
-import {View, Text} from 'react-native';
+import React, {useState} from "react";
+import {View, Image} from 'react-native';
+import { Button } from "react-native-paper";
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker'
 
 const HomePage = () => {
+    const [image, setImage] = useState();
+    const onTakePhoto = () => launchCamera({mediaType:'photo'}, onImageSelect)
+    const onSelectImagePress = () => launchImageLibrary({mediaType: 'photo', includeBase64: true}, onImageSelect)
+
+    const onImageSelect = async (media) => {
+      if(!media.didCancel){
+        console.log(media.didCancel)
+        setImage(media.assets.uri);
+      } else {console.log('failed')}
+    };
+
     return (
         <View>
-            <Text>This is HomePage</Text>
+            <Button onPress={onTakePhoto}>Capture Image</Button>
+            <Button onPress={onSelectImagePress}>Pick Image</Button>
+            <Image source={{uri:image}} resizeMode ='contain'/>
         </View>
     )
 }
